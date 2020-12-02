a1d442c57b4ded19dea68a613c767025ef4a83c2
chiaragualdi <73993515+chiaragualdi@users.noreply.github.com>
2020-12-02 22:37:36 +0100

    Update es29p73.py

--- es29p73.py
+++ es29p73.py
@@ -1,45 +1,32 @@
-from random import randint
 listacitta=[]
-listatempmax=[]
-listatempmin=[]
-escursionemax=randint(1, 100)
-parte1=True
-while parte1==True:
+listatmax=[]
+listatmin=[]
+dacambiare=0
+n=0
+while True:
     n+=1
-    print("Inserisci il nome della città:")
+    print("Inserisci il nome della città numero", n)
     nomecitta=input()
-    listacitta.append(nomecitta)
-    print("Inserisci il valore massimo dell'escursione termica di", nomecitta)
-    etmax = int(input())
-    listatempmax.append(etmax)
-    print("Inserisci il valore minimo dell'escursione termica di", nomecitta)
-    etmin = int(input())
-    listatempmin.append(etmin)
-    fermo=input("Se hai terminato le città da inserire, digita stop, altrimenti qualsiasi altro valore: ")
-    if fermo=="stop":
-        parte1=False
+    listacittà.append(nomecitta)
+    print("Inserisci il valore massimo della temperatura giornaliera di", nomecitta)
+    vmaxt=int(input())
+    listatmax.append(vmaxt)
+    print("Digita il valore minimo della temperatura giornaliera di", nomecitta)
+    vmint=int(input())
+    listatmin.append(vmint)
+    fine=input("Inserisci fine se hai finito le città da inserire, inserisci qualsiasi altra cosa per continuare ")
+    if fine== "fine":
+        break
     else:
         pass
-parte2=True
-while parte2==True:
-    print("Scegli un indice presente nelle 3 liste per selezionare una città con la sua escursione termica: ")
-    indice=int(input())
-    escursionetermica=etmax[indice]-etmin[indice]
-    diffescursione=escursionemax-escursionetermica
-    print("Valore massimo del contatore: ", escursionemax)
-    if diffescursione>0:
-        print("L'escursione termica di", listacittà[indice],"è di", escursionetermica,"°. E' compresa nel range che avevamo prefissato per", diffescursione,"°.")
-        parte2=False
+
+print("Inserisci il valore massimo dell'escursione termica che può accettare il contatore")
+escursionemax=int(input())
+for i in range(n):
+    escursionecittà = listatmax[i]-listatmin[i]
+    if escursionemax<escursionecittà:
+        dacambiare+=1
+        print("I valori delle città che non rientrano nel range del contatore sono:", dacambiare)
     else:
-        print("L'escursione termica di", città[indice],"è di", escursionetermica,"°. Non è compresa nel range che avevamo prefissato per", diffescursione,"°.")
-        parte3=True
-        while parte3==True:
-            print("Adesso alzerò il range del contatore per far entrare l'escursione termica di", città[indice], "nel range prefissato.")
-                diffescursione+=1
-                if diffescursione>0:
-                    print("Adesso i valori rientrano nel range.")
-                        parte1 = False
-                        parte2 = False
-                        parte3 = False
-                else:
-                    pass
+        pass
+print("Ora tutti i valori  delle città rientrano nel range del contatore")
