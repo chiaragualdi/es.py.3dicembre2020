4c61dbb04aae473a16bbc7772b7052834e92d754
chiaragualdi <73993515+chiaragualdi@users.noreply.github.com>
2020-12-03 09:01:53 +0100

    Update es30pag73.py

--- es30pag73.py
+++ es30pag73.py
@@ -1,7 +1,7 @@
 lunghezza=int(input("Inserisci il numero di cifre da cui è composto il numero binario: "))
 somma=0
 for i in range(lunghezza):
-    cifra=int(input("elenca le cifre a partire da destra"))
-    valore=(cifra*cifra)
+    cifra=int(input("Elenca le cifre a partire da destra: "))
+    valore=(cifra*2**i)
     somma=somma+valore
 print(somma)
